94796755d3a4faba92c93bfdee91f95684b044b2
m.daghestani <m.daghestani@asanpardakht.ir>
2018-04-29 19:29:40 +0430

    Add project files.

---
+++ MRD.OwinMiddlewares.NLog/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRD.OwinMiddlewares.NLog
+{
+    public static class Extensions
+    {
+        public static IAppBuilder UseNLog(this IAppBuilder app)
+        {
+            global::NLog.LogManager.Configuration
+
+            return app.NotifyRequestInfoWithNLog()
+                .UseExceptionLoggerWithNLog()
+                .UseRequestBodyLoggerWithNLog()
+                .UseResponseBodyLoggerWithNLog();
+        }
+        public static IAppBuilder NotifyRequestInfoWithNLog(this IAppBuilder app)
+        {
+            return app.NotifyRequestInfo(info =>
+            {
+                global::NLog.MappedDiagnosticsLogicalContext.Set("RequestId", info.Id);
+                global::NLog.MappedDiagnosticsLogicalContext.Set("URL", info.Uri.ToString());
+                global::NLog.MappedDiagnosticsLogicalContext.Set("Method", info.Method);
+                global::NLog.MappedDiagnosticsLogicalContext.Set("QueryString", info.QueryString);
+                global::NLog.MappedDiagnosticsLogicalContext.Set("LocalIpAddress", info.LocalIpAddress);
+                global::NLog.MappedDiagnosticsLogicalContext.Set("RemoteIpAddress", info.RemoteIpAddress);
+                global::NLog.MappedDiagnosticsLogicalContext.Set("XForwardedFor", info.XForwardedFor);
+
+            });
+        }
+        public static IAppBuilder UseRequestBodyLoggerWithNLog(this IAppBuilder app)
+        {
+            return app.UseRequestBodyLogger(new NLogLogger());
+        }
+        public static IAppBuilder UseResponseBodyLoggerWithNLog(this IAppBuilder app)
+        {
+            return app.UseResponseBodyLogger(new NLogLogger());
+        }
+        public static IAppBuilder UseExceptionLoggerWithNLog(this IAppBuilder app)
+        {
+            return app.UseExceptionLogger(new NLogLogger());
+        }
+    }
+}
---
+++ MRD.OwinMiddlewares.NLog/NLogLogger.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRD.OwinMiddlewares.NLog
+{
+    public class NLogLogger : ILogger
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        public bool IsDebugEnabled => _logger.IsDebugEnabled;
+
+        public bool IsFatalEnabled => _logger.IsFatalEnabled;
+
+        public void Debug(string log)
+        {
+            _logger.Debug(log);
+        }
+
+        public void Fatal(Exception exception)
+        {
+            _logger.Fatal(exception);
+        }
+    }
+}
